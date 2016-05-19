d70c4b7ad6598b6d5af9f417d6d72bf478ef4072
zjohnsonedtech <zac@z-johnson.com>
2016-05-19 10:28:22 -0500

    Update app.js
    remove '.current' test

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 var main = function(){
-	$('.article').removeClass('current');
 	$('.post').hide();
 	$('#hide').hide();
 
-
 	$('#expand').click(function(){
-		$(this).parentUntil(article).addClass('current');
-    		$('.current').children('.excerpt').hide();
-    		$('.current').children('#expand').hide();
-    		$('.current').children('.post').show();
-    		$('.current').children('#hide').show();
-    	
+		$('.excerpt').hide();
+		$('#expand').hide();
+		$('.post').show();
+		$('#hide').show();
 	});
 
 	$('#hide').click(function(){
-		$('.current').children('.post').hide();
-		$('.current').children('#hide').hide();
-		$('.current').children('.excerpt').show();
-		$('.current').children('#expand').show();
+		$('.post').hide();
+		$('#hide').hide();
+		$('.excerpt').show();
+		$('#expand').show();
 	});
 
 
