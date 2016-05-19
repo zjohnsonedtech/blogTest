3f5042c734e2a1b57a27007c3130712983277beb
zjohnsonedtech <zac@z-johnson.com>
2016-05-19 11:41:58 -0500

    Update app.js
    remove '.current' test round 2

--- app.js
+++ app.js
@@ -3,23 +3,17 @@ var main = function(){
 	$('.hide').hide();
 
 	$('.expand').click(function(){
-		$('.article').removeClass('current');
-		$(this).parentsUntil('.blogPost').addClass('current');
-		
-		
-		$('.current').children('.excerpt').hide();
-		$('.current').children('.expand').hide();
-		$('.current').children('.post').show();
-		$('.current').children('.hide').show();
+		$('.excerpt').hide();
+		$('.expand').hide();
+		$('.post').show();
+		$('.hide').show();
 	});
 
 	$('.hide').click(function(){
-		$('.current').children('.post').hide();
-		$('.current').children('.hide').hide();
-		$('.current').children('.excerpt').show();
-		$('.current').children('.expand').show();
-		
-		$(this).parentsUntil('.blogPost').removeClass('current');
+		$('.post').hide();
+		$('.hide').hide();
+		$('.excerpt').show();
+		$('.expand').show();
 	});
 
 
